59e89ce576e7cdd79db6ddd541caa6dcc5422bd3
Hussein-osama <so7hs.oh@gmail.com>
2024-07-25 03:10:34 +0300

    update followers counter

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,7 +3,14 @@ function increaseCount() {
     count++; // Increment the count by 1
     displayCount(); // Display the count
     checkCountValue(); // Check count value and display messages
-  }
+    document.getElementById("countDisplay").textContent = count;
+}
+
+function resetCount() {
+  count = 0;
+  document.getElementById("countDisplay").textContent = count;
+  alert("The Followers count has been reset.");
+}
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
     }
